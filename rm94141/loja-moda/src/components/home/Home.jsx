6fa2fd81012944ee3c9179e7725dfe363042a7d5
Erik Hakamada <erik.hakamada@hotmail.com>
2022-08-26 19:56:12 -0300

    Adicionando conteúdo aos componentes Home e Sobre

--- rm94141/loja-moda/src/components/home/Home.jsx
+++ rm94141/loja-moda/src/components/home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 export default function Home() {
-    
+
   return (
     <div className='home'>
-        <h1></h1>
-        <p>Exemplo da página principal do projeto.</p>
+        <h1>Nike da Shopee</h1>
+        <p className="paragrafo">Essa é uma loja com produtos da "Nike"</p>
+        <p className="paragrafo">Trabalhamos com moda masculina, feminina e infantil</p>
     </div>
   )
 }
--- rm94141/loja-moda/src/components/sobre/Sobre.jsx
+++ rm94141/loja-moda/src/components/sobre/Sobre.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Sobre(){
     return (
         <div className='home'>
-            <ul>
-                <li>RM94141 - Mariana Nieton Borges</li>
-            </ul>
+            <h1>Sobre nós</h1>
+            <p className="paragrafo">Loja criada pela aluna Mariana Nieton Borges da FIAP</p>
+            <p className="paragrafo">RM 94141</p>
         </div>
     )
 }
